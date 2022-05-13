1e739e74b7a28de115bd86420631c8f5eb0c10a0
Quin Marilyn <quin.marilyn05@gmail.com>
2022-05-13 17:19:21 -0600

    Lint.

--- globalPlugins/cliptools/clip_handler.py
+++ globalPlugins/cliptools/clip_handler.py
@@ -21,12 +21,14 @@ GlobalUnlock = ctypes.windll.kernel32.GlobalUnlock
 GMEM_DDESHARE = 0x2000
 CF_TEXT = 1
 
+
 def get_text():
 	OpenClipboard(None)
 	pcontents = GetClipboardData(CF_TEXT)
 	data = ctypes.c_char_p(pcontents).value
 	CloseClipboard()
 	return data
+
 
 def set_text(data):
 	OpenClipboard(None)
